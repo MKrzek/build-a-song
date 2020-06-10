2998958d68c81ed7e41255aa8fa5d0fbc8c4a6d3
MKrzek <mkrzek@googlemail.com>
2020-06-10 14:56:12 +0100

    feat: implemented passport

--- server/server.js
+++ server/server.js
@@ -2,13 +2,33 @@
 const express = require('express');
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
-
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
 
 const app = express();
 
 app.use(bodyParser.json());
+const db = mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'aaabbbccc',
+    store: new MongoStore({
+      mongooseConnection: db,
+    }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(
   '/graphql',
   expressGraphQL({
