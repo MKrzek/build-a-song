a2e752473955c370d2e356e14b04bbe1cd0c2cde
MKrzek <mkrzek@googlemail.com>
2020-06-10 14:59:16 +0100

    feat: add user-type and user-input-type

---
+++ server/schema/user_type.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const graphql = require('graphql');
+
+const {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+
+const User = mongoose.model('user');
+
+const UserType = new GraphQLObjectType({
+  name: 'UserType',
+  fields: () => ({
+    email: { type: GraphQLString },
+  }),
+});
+
+const UserInputType = new GraphQLInputObjectType({
+  name: 'UserSignupInputType',
+  fields: () => ({
+    email: { type: new GraphQLNonNull(GraphQLString) },
+    password: { type: new GraphQLNonNull(GraphQLString) },
+  }),
+});
+module.exports = { UserType, UserInputType };
