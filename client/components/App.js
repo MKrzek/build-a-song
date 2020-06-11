e8457d56305726a9c2eca14310ed9f46a7a77c8f
MKrzek <mkrzek@googlemail.com>
2020-06-11 14:15:54 +0100

    feat: wired up user query to get a logged-in user

--- client/components/App.js
+++ client/components/App.js
@@ -1,26 +1,39 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { useQuery } from 'react-apollo';
 
-const App = ({ children }) => (
-  <div>
-    <nav>
-      <div className="nav-wrapper">
-        <ul>
-          <li>
-            <Link to="/login">Log-in</Link>
-          </li>
+const AUTH_USER = gql`
+  query AUTH_USER {
+    user {
+      email
+      id
+    }
+  }
+`;
 
-          <li>
-            <Link to="/signin">Sign-in</Link>
-          </li>
-          <li>
-            <Link to="/logout">Logout</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <div>{children} </div>
-  </div>
-);
+const App = ({ children }) => {
+  const { loading, error, data } = useQuery(AUTH_USER);
+  console.log('data', data);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!data.user === null) return <p>Please log in</p>;
+
+  return (
+    <div>
+      <nav>
+        <div className="nav-wrapper">
+          <ul>
+            <li>{!data.user && <Link to="/login">Log-in</Link>}</li>
+
+            <li>{!data.user && <Link to="/signin">Sign-in</Link>}</li>
+            <li>{data.user && <Link to="/logout">Logout</Link>}</li>
+          </ul>
+        </div>
+      </nav>
+      <div>{children} </div>
+    </div>
+  );
+};
 export default App;
