7792c61cebf352294977fa6096fd54742e18fb96
MKrzek <mkrzek@googlemail.com>
2020-06-12 15:21:12 +0100

    feat: wired up logout mutation

--- client/components/Logout.js
+++ client/components/Logout.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from 'react-apollo';
+import { AUTH_USER } from './App';
 
-const Logout = () => <div>You have successfully logged out</div>;
+const LOGOUT = gql`
+  mutation LOGOUT {
+    logout {
+      email
+      id
+    }
+  }
+`;
+
+const Logout = () => {
+  const [logout, { loading, error }] = useMutation(LOGOUT, {
+    refetchQueries: [{ query: AUTH_USER }],
+  });
+  useEffect(() => {
+    logout();
+  }, [logout]);
+  return <h4>You have successfully logged out</h4>;
+};
 
 export default Logout;
